f7c8d781970a96140af18eb6bd40a30706fbe332
Dina Peikr <divo1peikr@gmail.com>
2021-03-18 21:46:48 +0400

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatDrawer} from '@angular/material/sidenav';
+import {UnSubscriber} from './utils/unsubscriber';
+import {Observable} from 'rxjs';
+import {MatCheckboxChange} from '@angular/material/checkbox';
+import {IProduct, ProductsService} from './services/products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    // {
+    //   provide: ProductsService,
+    // }
+  ]
 })
-export class AppComponent {
-  title = 'ang-theory';
+
+export class AppComponent extends UnSubscriber implements OnInit {
+  public title!: string;
+  public drawerFromApp!: MatDrawer;
+  // public products: IProduct[] =  products;
+  public products$: Observable<IProduct[]> = this.productsServer.getProductsJson();
+  // public se$: Observable<boolean | null> = products$
+  //   .pipe(
+  //     pluck('serverError')
+  //     // map((res) => res.serverError)
+  //   );
+  public searchText = '';
+  public hasFavorites = false;
+  // constructor(private cdr: ChangeDetectorRef) {
+  // }
+
+  constructor(private productsServer: ProductsService) {
+    super();
+    console.log(this.productsServer.timestamp);
+  }
+
+  ngOnInit(): void {
+    this.title = 'Angular-learn-ru';
+    // const subProducts =
+    // products$
+    //   .pipe(takeUntil(this.UnSubscriber$))
+    //   .subscribe(({products}) => {
+    //   this.products$ = products;
+    // });
+
+    // this.sub.push(subProducts);
+    // console.log(this.productsServer);
+    // console.log(this.products$);
+  }
+
+  public setDrawer(drawer: MatDrawer): void {
+    // console.log('APP', drawer);
+    // setTimeout(() => {
+    //   this.drawerFromApp = drawer;
+    // }, 100);
+    // Promise.resolve().then(() => {
+    //   this.drawerFromApp = drawer;
+    // });
+
+    this.drawerFromApp = drawer;
+    // this.cdr.detectChanges();
+  }
+
+  public searchProductByTitle(text: string): void {
+    console.log(text);
+    this.searchText = text;
+  }
+
+  public productsFilter(products: IProduct[]): IProduct[] {
+    return products.filter((product: IProduct) => {
+      return `${product.title} ${product.price}`.toLocaleLowerCase().includes(this.searchText.toLowerCase());
+    });
+  }
+
+  toggleFavoriteProducts(event: MatCheckboxChange): void {
+    this.hasFavorites = event.checked;
+    console.log(this.hasFavorites);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,64 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ExampleModule} from './example/example.module';
+import {HeaderComponent} from './header/header.component';
+import {SideMenuComponent} from './side-menu/side-menu.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SharedModule} from './shared/shared.module';
+import {FooterComponent} from './footer/footer.component';
+import {ShopCardComponent} from './shop-card/shop-card.component';
+import {ProductsFilterPipe} from './pipes/products-filter.pipe';
+import {FormsModule} from '@angular/forms';
+import {ExchangeRatesComponent} from './header/exchange-rates/exchange-rates.component';
+import {ExchangeRatesDirective} from './header/exchange-rates/exchange-rates.directive';
+import {HiddenDirective} from './header/exchange-rates/hidden.directive';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {BASE_URL_TOKEN, baseUrl} from './services/config';
+import {InterceptorsService} from './services/interceptors.service';
+import {ModalModule} from './modal/modal.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideMenuComponent,
+    FooterComponent,
+    ShopCardComponent,
+    ProductsFilterPipe,
+    ExchangeRatesComponent,
+    ExchangeRatesDirective,
+    HiddenDirective,
   ],
   imports: [
-    BrowserModule
+    SharedModule,
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ExampleModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    // {
+    //   provide: ProductsService,
+    //   useClass: ProductsService
+    // }
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorsService,
+      multi: true
+    },
+    {
+      provide: BASE_URL_TOKEN,
+      useValue: baseUrl,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
